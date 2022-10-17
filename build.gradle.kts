748bd46785d7cf41e2cce6ce08c5eca2ac6ba587
berik <berik@mobile.dev>
2022-10-17 17:48:38 +0200

    Prepare for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,26 @@
+import com.vanniktech.maven.publish.MavenPublishPluginExtension
+import com.vanniktech.maven.publish.SonatypeHost
+import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
+
 buildscript {
     repositories {
         mavenCentral()
     }
     dependencies {
         classpath(kotlin("gradle-plugin", "1.5.30"))
+        classpath("com.vanniktech:gradle-maven-publish-plugin:0.17.0")
     }
 }
 
 repositories {
     google()
     mavenCentral()
+}
+
+allprojects {
+    pluginManager.withPlugin("com.vanniktech.maven.publish") {
+        extensions.getByType(MavenPublishPluginExtension::class.java).apply {
+            sonatypeHost = SonatypeHost.S01
+        }
+    }
 }
